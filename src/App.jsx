147659c4d88aa04731f726e93979e3fc43747120
stringedcat <mauroox09@gmail.com>
2021-11-30 23:34:24 -0300

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Post from "./pages/Post/Post";
 import Login from "./pages/Login/Login";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 const App = () => {
+  const user = false;
   return (
     <BrowserRouter>
       <div className="app">
-        <Navbar />
-        <Routes></Routes>
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/posts/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <div className="navbar">
-      <span className="logo">Lama App</span>
-      <ul className="list">
-        <li className="listItem">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Userbox_creeper.svg/1200px-Userbox_creeper.svg.png"
-            alt=""
-            className="avatar"
-          />
-        </li>
-        <li className="listItem">Mauro Loyola</li>
-        <li className="listItem">Logout</li>
-      </ul>
+      <span className="logo">
+        <Link className="link" to="/">
+          Lama App
+        </Link>
+      </span>
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Userbox_creeper.svg/1200px-Userbox_creeper.svg.png"
+              alt=""
+              className="avatar"
+            />
+          </li>
+          <li className="listItem">Mauro Loyola</li>
+          <li className="listItem">Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
