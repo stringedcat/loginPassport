4f2aa0f606242aac1517ad4c0871a542ec5dd682
stringedcat <mauroox09@gmail.com>
2021-12-02 00:12:09 -0300

    updated profile

---
+++ client/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Post from "./pages/Post/Post";
+import Login from "./pages/Login/Login";
+import axios from "axios";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("authentication has been failed!");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getUser();
+  }, []);
+
+  console.log(user);
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
