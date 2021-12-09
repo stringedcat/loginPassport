2a8c003f35d825a47e2a2d7a27293957096da7a7
Mauro Loyola <mauroox09@gmail.com>
2021-12-09 02:11:18 -0300

    added facebook

--- backend/passport.js
+++ backend/passport.js
@@ -1,12 +1,14 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GitHubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 const GOOGLE_CLIENT_ID =
   "319634715691-5f2lqpjlt3f0i87lkg6tuht6edp5tpoh.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
 const GITHUB_CLIENT_ID = "70221375e0f632901cd9";
 const GITHUB_CLIENT_SECRET = "16ba0287e901869dadba936821965705e3aa5c19";
-
+const FACEBOOK_APP_ID = "377648377477041";
+const FACEBOOK_APP_SECRET="865dc75e6ce72e825ff39c7e12f7d1b5";
 passport.use(
   new GoogleStrategy(
     {
@@ -31,6 +33,19 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new FacebookStrategy(
+    {
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+
 /* passport.use(
   new GitHubStrategy(
     {
